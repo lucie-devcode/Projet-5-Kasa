3493cc16416a86523d9d8fff4029180b13df1a21
lucie-devcode <lucie.devcode@gmail.com>
2025-12-19 17:53:48 +0100

    correction grille appartements

--- src/Components/ApartmentGrid.jsx
+++ src/Components/ApartmentGrid.jsx
@@ -10,7 +10,6 @@ function ApartmentGrid() {
           <div key={logement.id} className="apartment-card">
             <img src={logement.cover} alt={logement.title} />
             <h3>{logement.title}</h3>
-            <p>{logement.location}</p>
           </div>
         ))}
       </div>
