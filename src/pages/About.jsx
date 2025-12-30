6a9be559194c9b142dc0264b725426baa2fb4cd2
lucie-devcode <lucie.devcode@gmail.com>
2025-12-30 14:34:08 +0100

    Corrections textes pages about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,21 +7,23 @@ function About() {
   const aboutData = [
     {
       title: "Fiabilité",
-      content: "Les annonces postées sur Kasa sont fiables et vérifiées.",
+      content:
+        "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photo sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.",
     },
     {
       title: "Respect",
       content:
-        "La bienveillance fait partie des valeurs fondamentales de Kasa.",
+        "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoires ou de perturbation du voisinage entraînera une exclusion de notre plateforme.",
     },
     {
       title: "Service",
       content:
-        "Nos équipes sont disponibles pour répondre à toutes vos questions.",
+        "La qualité du service est au cœur de notre engagement chez Kasa. Nous veillons à ce que chaque interaction, que ce soit avec nos hôtes ou nos locataires, soit empreinte de respect et de bienveillance.",
     },
     {
       title: "Sécurité",
-      content: "La sécurité est notre priorité pour garantir un séjour serein.",
+      content:
+        "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité érablis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
     },
   ];
 
